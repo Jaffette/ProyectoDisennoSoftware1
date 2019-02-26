8bb5f3cd56eae393173435f6552cc459ea25d2aa
Jaffette <jaffette@outlook.com>
2019-02-25 21:37:36 -0600

    feat: pagina para crear el juego

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DecisionComponent } from './decision/decision.component';
+import { CreateGameComponent } from './create-game/create-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DecisionComponent
+    DecisionComponent,
+    CreateGameComponent
   ],
   imports: [
     BrowserModule,
