c2fcf809816ba41878199fea7d41394d89d29dee
unknown <uri.arte08@gmail.com>
2019-03-09 20:37:59 -0600

    CPequeñas modificaciones en el decision.component

--- src/app/components/decision/decision.component.ts
+++ src/app/components/decision/decision.component.ts
@@ -10,7 +10,7 @@ import { UserService } from '../../services/user.service';
 export class DecisionComponent implements OnInit {
 
 
-  active:boolean=true;
+  active = true;
 
   constructor(public user: UserService) { }
 
